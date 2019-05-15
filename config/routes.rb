09be912b0192b346237b909e7c7e69d011895c72
alice de chalendar <alice.dechalendar@gmail.com>
2019-05-15 15:34:17 +0100

    created restaurant-review model, lacking the embedding of the review form inside each restaurant page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # namespace :admin do
+  #   resources :restaurants, only: [:index, :destroy]
+  # end
+
+  resources :restaurants, except: [:destroy, :update] do
+    resources :reviews, only: [:new, :create]
+  end
+  # get 'restaurants', to: 'restaurants#index'
+
+  # get 'restaurants/new', to: 'restaurants#new'
+  # post 'restaurants', to: 'restaurants#create'
+
+  # get 'restaurants/:id', to: 'restaurants#show'
+
+  # get 'restaurants/:id/reviews/new'
+  # post 'restaurants/:id/reviews'
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,38 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'Taverne de Zhao',
+    address:      'Paris 10',
+    category:     'chinese'
+  },
+  {
+    name:         'Big Mamma',
+    address:      'Paris 9',
+    category:     'italian'
+  },
+  {
+    name:         'le 52',
+    address:      'Paris 10',
+    category:     'french'
+  },
+  {
+    name:         'Matsuri',
+    address:      'Paris 16',
+    category:     'japanese'
+  },
+  {
+    name:         'Chez Graf',
+    address:      'Paris 7',
+    category:     'french'
+  }
+]
+
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
